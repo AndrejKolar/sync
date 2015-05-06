041c126d079141c6678fa443eaba5e71194dce66
Andrej Kolar <andrej.kolar@me.com>
2015-05-06 11:46:50 +0200

    First commit

---
+++ sync.rb
@@ -0,0 +1,30 @@
+require 'net/ftp'
+require 'io/console'
+
+SERVER = "andrejkolar.synology.me"
+ACCOUNT = "admin"
+LOCAL_FOLDER = "/Users/andrejkolar/Downloads/"
+REMOTE_FOLDER = "/Watch/"
+
+# Password
+puts "Password:"
+password = STDIN.noecho(&:gets).chomp
+
+Net::FTP.open(SERVER, ACCOUNT, password) do |ftp|
+  ftp.chdir(REMOTE_FOLDER)
+  files = ftp.list
+
+  Dir.foreach(LOCAL_FOLDER) do |file|
+    next if file.start_with?('.')
+    next unless file.end_with?('.torrent')
+
+    # Upload
+    p "Uploading file " + file.to_s
+    ftp.putbinaryfile(LOCAL_FOLDER + file)
+
+    # Delete
+    filePath = File.join(LOCAL_FOLDER, file)
+    File.delete(filePath)
+
+  end
+end
